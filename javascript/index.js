79bfb2a6b7ef2b47a7af2317b70969f8a7181df8
HFMolnar <142211570+HFMolnar@users.noreply.github.com>
2023-10-10 19:50:45 +0100

    Added current location functionality

--- javascript/index.js
+++ javascript/index.js
@@ -34,6 +34,9 @@ function displayTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#allCities");
