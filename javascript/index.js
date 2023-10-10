3e1956d4cd7463b619cfff4c47a3512b8e86edba
HFMolnar <142211570+HFMolnar@users.noreply.github.com>
2023-10-10 19:42:25 +0100

    Added timezone values, created a function in js to update city when selected from dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,54 @@
 function displayTime() {
   let londonElement = document.querySelector("#london");
-  let londonElementDate = londonElement.querySelector(".date");
-  let londonElementTime = londonElement.querySelector(".time");
+  if (londonElement) {
+    let londonElementDate = londonElement.querySelector(".date");
+    let londonElementTime = londonElement.querySelector(".time");
 
-  londonElementDate.innerHTML = moment().format("dddd Do MMMM YYYY");
-  londonElementTime.innerHTML = moment().format("HH:mm:ss");
+    let londonLocal = moment().tz("Europe/London");
+    londonElementDate.innerHTML = londonLocal.format("dddd Do MMMM YYYY");
+    londonElementTime.innerHTML = londonLocal.format("HH:mm:ss");
+  }
+  // NY
+
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkElementDate = newYorkElement.querySelector(".date");
+    let newYorkElementTime = newYorkElement.querySelector(".time");
+    let newYorkLocal = moment().tz("America/New_York");
+
+    newYorkElementDate.innerHTML = newYorkLocal.format("dddd Do MMMM YYYY");
+    newYorkElementTime.innerHTML = newYorkLocal.format("HH:mm:ss");
+  }
+  // Zurich
+
+  let zurichElement = document.querySelector("#zurich");
+  if (zurichElement) {
+    let zurichElementDate = zurichElement.querySelector(".date");
+    let zurichElementTime = zurichElement.querySelector(".time");
+    let zurichLocal = moment().tz("Europe/Zurich");
+
+    zurichElementDate.innerHTML = zurichLocal.format("dddd Do MMMM YYYY");
+    zurichElementTime.innerHTML = zurichLocal.format("HH:mm:ss");
+  }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#allCities");
+  citiesElement.innerHTML = `
+    <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd Do MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>`;
+}
+
+displayTime();
 setInterval(displayTime, 1000);
+
+let citiesSelectorElement = document.querySelector("#city-selector");
+citiesSelectorElement.addEventListener("change", updateCity);
